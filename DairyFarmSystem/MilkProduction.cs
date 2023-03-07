60a421b723f13525dce95460eed0046db4836b52
saiedd01 <sa3edahmed20@gmail.com>
2023-03-07 15:00:53 +0200

    Change buttons name

--- DairyFarmSystem/MilkProduction.cs
+++ DairyFarmSystem/MilkProduction.cs
@@ -58,5 +58,10 @@ namespace DairyFarmSystem
             ns.Show();
             this.Hide();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
