e426d8e7c993b59d762518da9c33fa693d7ecb26
saiedd01 <sa3edahmed20@gmail.com>
2023-03-08 20:06:43 +0200

    Add Connection DataBase to Breeding Form

--- DairyFarmSystem/Breeding.cs
+++ DairyFarmSystem/Breeding.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -61,6 +62,13 @@ namespace DairyFarmSystem
             MilkSales ns = new MilkSales();
             ns.Show();
             this.Hide();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\Documents\DFarm.mdf;Integrated Security=True;Connect Timeout=30");
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
