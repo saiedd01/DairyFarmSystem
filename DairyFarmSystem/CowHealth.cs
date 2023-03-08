a8d37275b898930c7d293cae9c32c7ea1beaa4b0
saiedd01 <sa3edahmed20@gmail.com>
2023-03-08 09:24:25 +0200

    Connection DataBase to Form

--- DairyFarmSystem/CowHealth.cs
+++ DairyFarmSystem/CowHealth.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -57,6 +58,12 @@ namespace DairyFarmSystem
             DashBorad ns = new DashBorad();
             ns.Show();
             this.Hide();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\Documents\DFarm.mdf;Integrated Security=True;Connect Timeout=30");
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
