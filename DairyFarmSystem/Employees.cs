ab3c5ea1d3298d2e53ca7f282a26cd61cf23dc8f
saiedd01 <sa3edahmed20@gmail.com>
2023-03-13 15:27:42 +0200

    Handle Clear Btn

--- DairyFarmSystem/Employees.cs
+++ DairyFarmSystem/Employees.cs
@@ -92,5 +92,10 @@ namespace DairyFarmSystem
                 }
             }
         }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
     }
 }
