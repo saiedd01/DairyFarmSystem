7fd852874383553364d0b8d515e491eafd8942a8
saiedd01 <sa3edahmed20@gmail.com>
2023-03-14 19:54:44 +0200

    Create Populate Function

--- DairyFarmSystem/Finance.cs
+++ DairyFarmSystem/Finance.cs
@@ -95,6 +95,18 @@ namespace DairyFarmSystem
 
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\Documents\DFarm.mdf;Integrated Security=True;Connect Timeout=30");
 
+        private void populate()
+        {
+            con.Open();
+            string Query = "select * from ExpenditureTbl";
+            SqlDataAdapter sda = new SqlDataAdapter(Query, con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            ExpDGV.DataSource = ds.Tables[0];
+            con.Close();
+        }
+
         private void Save_Click(object sender, EventArgs e)
         {
 
