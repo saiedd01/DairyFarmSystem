eba66e11a2736f022df70f75bcee6784f3538277
saiedd01 <sa3edahmed20@gmail.com>
2023-03-20 22:58:08 +0200

    Coding Splash Form

--- DairyFarmSystem/Splash.Designer.cs
+++ DairyFarmSystem/Splash.Designer.cs
@@ -29,17 +29,19 @@ namespace DairyFarmSystem
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label21 = new System.Windows.Forms.Label();
-            this.bunifuProgressBar1 = new Bunifu.Framework.UI.BunifuProgressBar();
+            this.MyProgress = new Bunifu.Framework.UI.BunifuProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(308, 143);
+            this.pictureBox1.Location = new System.Drawing.Point(334, 142);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(142, 80);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -51,24 +53,28 @@ namespace DairyFarmSystem
             this.label21.AutoSize = true;
             this.label21.Font = new System.Drawing.Font("Century Gothic", 25F);
             this.label21.ForeColor = System.Drawing.Color.White;
-            this.label21.Location = new System.Drawing.Point(185, 23);
+            this.label21.Location = new System.Drawing.Point(208, 21);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(392, 51);
             this.label21.TabIndex = 52;
             this.label21.Text = "Dairy Farm System";
             // 
-            // bunifuProgressBar1
+            // MyProgress
             // 
-            this.bunifuProgressBar1.BackColor = System.Drawing.Color.Silver;
-            this.bunifuProgressBar1.BorderRadius = 5;
-            this.bunifuProgressBar1.Location = new System.Drawing.Point(0, 470);
-            this.bunifuProgressBar1.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
-            this.bunifuProgressBar1.MaximumValue = 100;
-            this.bunifuProgressBar1.Name = "bunifuProgressBar1";
-            this.bunifuProgressBar1.ProgressColor = System.Drawing.Color.Teal;
-            this.bunifuProgressBar1.Size = new System.Drawing.Size(822, 12);
-            this.bunifuProgressBar1.TabIndex = 53;
-            this.bunifuProgressBar1.Value = 0;
+            this.MyProgress.BackColor = System.Drawing.Color.Silver;
+            this.MyProgress.BorderRadius = 5;
+            this.MyProgress.Location = new System.Drawing.Point(0, 474);
+            this.MyProgress.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.MyProgress.MaximumValue = 100;
+            this.MyProgress.Name = "MyProgress";
+            this.MyProgress.ProgressColor = System.Drawing.Color.Teal;
+            this.MyProgress.Size = new System.Drawing.Size(823, 12);
+            this.MyProgress.TabIndex = 53;
+            this.MyProgress.Value = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Splash
             // 
@@ -76,7 +82,7 @@ namespace DairyFarmSystem
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkCyan;
             this.ClientSize = new System.Drawing.Size(823, 482);
-            this.Controls.Add(this.bunifuProgressBar1);
+            this.Controls.Add(this.MyProgress);
             this.Controls.Add(this.label21);
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Century Gothic", 13.8F);
@@ -85,6 +91,7 @@ namespace DairyFarmSystem
             this.Name = "Splash";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Splash";
+            this.Load += new System.EventHandler(this.Splash_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -95,6 +102,7 @@ namespace DairyFarmSystem
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label21;
-        private Bunifu.Framework.UI.BunifuProgressBar bunifuProgressBar1;
+        private Bunifu.Framework.UI.BunifuProgressBar MyProgress;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- DairyFarmSystem/Splash.cs
+++ DairyFarmSystem/Splash.cs
@@ -16,5 +16,26 @@ namespace DairyFarmSystem
         {
             InitializeComponent();
         }
+
+        int startppoint = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startppoint += 1;
+            MyProgress.Value = startppoint;
+            if(MyProgress.Value == 100)
+            {
+                MyProgress.Value = 0;
+                timer1.Stop();
+                Login Log = new Login();
+                this.Hide();
+                Log.Show();
+            }
+
+        }
+
+        private void Splash_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
     }
 }
